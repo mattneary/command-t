200d988945d3b07764fde36ffb043db58e13d3f1
Matt Neary <neary.matt@gmail.com>
2017-04-30 09:34:40 -0400

    add Result.js

---
+++ src/components/Result.js
@@ -0,0 +1,23 @@
+import {Component} from 'react'
+import cx from 'classnames'
+
+class Result extends Component {
+  render() {
+    const {title, url, match} = this.props.tab
+    const prefix = url.indexOf('//') === -1 ? 0 : url.indexOf('//') + 2
+    const [domain, ...path] = url.substr(prefix).split('/').filter(x => x)
+    return (
+      <li className={cx('result', this.props.active && 'active')}>
+        <span className='title'>{title}</span>
+        <span className='domain'>{domain}</span>
+        <div className='path-parts'>
+          {path.map((p, i) =>
+            <span key={i} className='path'>{p}</span>
+          )}
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Result
